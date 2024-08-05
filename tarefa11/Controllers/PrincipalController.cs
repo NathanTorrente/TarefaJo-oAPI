6c913c9e49ca01902c09a3341dbd21699f325d39
Nathan Rocha Torrente <2022102020009@aluno.ifro.edu.br>
2024-08-05 17:20:21 -0400

    1º Alter

---
+++ tarefa11/Controllers/PrincipalController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tarefa11.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class PrincipalController : ControllerBase
+    {
+
+        [HttpGet("/")]
+        public IActionResult Get()
+        {
+            return Ok("API Tarefas: online");
+        }
+
+        [HttpGet("Hello-World")]
+        public IActionResult GetWelloWorld()
+        {
+            return Ok("Hello world do João");
+        }
+      
+
+        [HttpGet("Autor")]
+        public IActionResult GetAutor() 
+        {
+        return Ok("Feito por UsuárioAnonimo");
+        }
+    }
+}
